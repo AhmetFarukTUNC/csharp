89c1da6e84cb7654c9ac95187c9d9b116557ee08
YAZILIMCIABII <atunc8402@gmail.com>
2024-08-01 17:24:26 +0300

    We defined a variable and print hello world and variable value.

---
+++ helloWorld/Program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.WriteLine("Hello, world!");
+System.Console.WriteLine("Press any key to exit..");
+System.Console.Read();
+
+// variables and write this variable.
+
+string hw = "Hello World";
+Console.WriteLine(hw);

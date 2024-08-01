8d02df6b1cf8fa41b8140ee066facdb57ea9ff06
YAZILIMCIABII <atunc8402@gmail.com>
2024-08-01 17:44:43 +0300

    We created alot of variables in different types and prin to console screen.

---
+++ variables/Program.cs
@@ -0,0 +1,62 @@
+﻿uint ui = 5U;
+
+int i = 5;
+
+sbyte sb = 127;
+
+decimal m = 30.5M;
+
+double d = 30.5D;
+
+float f = 30.5F;
+
+long l = 5L;
+
+ulong ul = 5UL;
+
+string s = "hello, this is a string literal";
+
+string sbs = @"The path is:
+C:\Windows\System32";//The backslashes and newline are included in the string
+
+char c = 'h';
+
+byte b = 127;
+
+short shortt = 127;
+
+ushort us = 127;
+
+bool booll = true;
+
+Console.WriteLine(ui);
+
+Console.WriteLine(i);
+
+Console.WriteLine(sb);
+
+Console.WriteLine(m);
+
+Console.WriteLine(d);
+
+Console.WriteLine(f);
+
+Console.WriteLine(l);
+
+Console.WriteLine(ul);
+
+Console.WriteLine(s);
+
+Console.WriteLine(sbs);
+
+Console.WriteLine(c);
+
+Console.WriteLine(b);
+
+Console.WriteLine(shortt);
+
+Console.WriteLine(us);
+
+Console.WriteLine(booll);
+
+Console.ReadLine();
